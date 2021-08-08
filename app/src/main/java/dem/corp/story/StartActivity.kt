dd93f8d87788a74f8e5dbc5057b11c287fd736b3
memaev <57516381+memaev@users.noreply.github.com>
2021-08-09 01:23:04 +0300

    Edit activity_start.xml

--- app/src/main/java/dem/corp/story/StartActivity.kt
+++ app/src/main/java/dem/corp/story/StartActivity.kt
@@ -1,9 +1,24 @@
 package dem.corp.story
 
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import dem.corp.story.databinding.ActivityStartBinding
+import dem.corp.story.repository.firebase.AUTH
+
 
 class StartActivity : AppCompatActivity() {
 
